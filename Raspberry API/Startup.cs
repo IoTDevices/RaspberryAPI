6b24d59f8b9e80cbb5fff39856bbe3584ce80554
Anthony Harrison <github@iotdevice.email>
2015-05-16 20:46:47 +0100

    Initial Commit

---
+++ Raspberry API/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Raspberry_API.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Raspberry_API.Startup))]
+namespace Raspberry_API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new SimpleAuthorizationServerProvider(),
+                RefreshTokenProvider = new SimpleRefreshTokenProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
---
+++ VSIX Installer/Readme.txt
@@ -0,0 +1,5 @@
+This .vsix file is a Visual Studio Extension which automatically imports the template into Visual Studio. To import it, merely double click it in Explorer.
+
+Once imported you will be able to merely go "File / New Project / CSharp / Raspberry API / (give the project a namespace)" and then press create.
+
+It will create exactly the code herein but namespaced to your selection when creating it.
